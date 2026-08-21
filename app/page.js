d68bf07d7a1236b53162e151b1c734ec3a564849
ManauTunjae <manau.tunjae@hotmail.com>
2026-08-21 10:55:28 +0200

    style: bg/color and nav/color

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Header from "../components/Header.jsx";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex min-h-screen flex-col bg-zinc-50 font-sans">
       <header className="w-full">
         <Header />
       </header>
--- components/Header.jsx
+++ components/Header.jsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <nav className="mx-auto flex w-full justify-between items-center px-4 py-4 sm:px-6">
+    <nav className="mx-auto flex w-full justify-between  items-center px-4 py-4 sm:px-6 bg-gray-300 shadow-lg">
       <div className="flex justify-between items-center gap-5">
         <Image src="/logo.svg" alt="my logo" width={50} height={50} />
         <span className="px-3 py-2 rounded-2xl bg-zinc-100/60 text-black font-bold">
           Manau Tunjae
         </span>
       </div>
-      <div className="flex jestify-between items-center gap-10 font-semibold text-md text-zinc-400 px-5 py-5">
+      <div className="flex jestify-between items-center gap-10 font-semibold text-md text-zinc-900 px-5 py-5">
         <Link className="hover:text-white transition-colors" href="/">
           Home
         </Link>
