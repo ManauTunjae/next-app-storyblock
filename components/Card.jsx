b0e000dcd055b5977994359e2aac15ee7798d456
ManauTunjae <manau.tunjae@hotmail.com>
2026-08-21 11:30:15 +0200

    style a card

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,9 +1,9 @@
 export default function Card({ title, description }) {
   return (
     <div>
-      <div>
-        <h3>{title}</h3>
-        <p>{description}</p>
+      <div className="max-w-sm rounded-lg border border-gray-200 bg-white p-6 shadow-md">
+        <h3 className="mb-2 text-xl font-bold tracking-tight text-gray-955">Title: {title}</h3>
+        <p className="mb-4 font-normal text-gray-700">{description}</p>
       </div>
     </div>
   );
