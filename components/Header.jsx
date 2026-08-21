af04c23b0fe0bce843b57dbdffd5a1d30fff42fa
ManauTunjae <manau.tunjae@hotmail.com>
2026-08-21 10:46:41 +0200

    feat: implement logo,name and link to pages

---
+++ components/Header.jsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <nav className="mx-auto flex w-full justify-between items-center px-4 py-4 sm:px-6">
+      <div className="flex justify-between items-center gap-5">
+        <Image src="/logo.svg" alt="my logo" width={50} height={50} />
+        <span className="px-3 py-2 rounded-2xl bg-zinc-100/60 text-black font-bold">
+          Manau Tunjae
+        </span>
+      </div>
+      <div className="flex jestify-between items-center gap-10 font-semibold text-md text-zinc-400 px-5 py-5">
+        <Link className="hover:text-white transition-colors" href="/">
+          Home
+        </Link>
+        <Link className="hover:text-white transition-colors" href="about">
+          About
+        </Link>
+        <Link className="hover:text-white transition-colors" href="contact">
+          Contact
+        </Link>
+        <Link className="hover:text-white transition-colors" href="jobs">
+          Jobs
+        </Link>
+      </div>
+    </nav>
+  );
+}
