93f74193fe9ffb84af43c1b0edb58a07332ddf48
ManauTunjae <manau.tunjae@hotmail.com>
2026-08-21 09:58:06 +0200

    feat: implement a header and description in contact page

---
+++ app/contact/page.js
@@ -0,0 +1,13 @@
+export default function Contact() {
+  return (
+    <div>
+      <header></header>
+      <main className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+        <h1>This is a Contact page</h1>
+        <article>Tel: 070-792 88 03</article>
+        <article>Email: manau.tunjae@yh.nackademin.se</article>
+      </main>
+      <footer></footer>
+    </div>
+  );
+}
