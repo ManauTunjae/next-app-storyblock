3b5c4b688043deb1cca486c742adbfd4b680c087
ManauTunjae <manau.tunjae@hotmail.com>
2026-08-21 10:21:52 +0200

    feat: import Header.jsx

--- app/page.js
+++ app/page.js
@@ -1,6 +1,13 @@
+import Header from "../components/Header.jsx";
+
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <header>
+        <Header />
+      </header>
+      <main></main>
+      <footer></footer>
     </div>
   );
 }
