dd28ecc38ae09fa34812ade40950c3104a1aa2fd
ManauTunjae <manau.tunjae@hotmail.com>
2026-08-21 11:28:50 +0200

    style a button "Click Me!"

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,6 +1,6 @@
 export default function Button({ text }) {
   return (
-    <div>
+    <div className="w-fit px-4 py-2 bg-stone-500 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded-lg transition duration-200">
       <button>{text}</button>
     </div>
   );
