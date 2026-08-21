e6d326ee5fd393f7282cc69db2c96931e57b83c3
ManauTunjae <manau.tunjae@hotmail.com>
2026-08-21 11:02:19 +0200

    feat: implement card component and recieve title and description props

---
+++ components/Card.jsx
@@ -0,0 +1,10 @@
+export default function Card({ title, description }) {
+  return (
+    <div>
+      <div>
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
