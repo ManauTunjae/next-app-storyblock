bd834bfeabd953efabb59e9dcec0056e09b44d0d
ManauTunjae <manau.tunjae@hotmail.com>
2026-08-21 10:01:12 +0200

    feat: implement a header and description in jobs page

---
+++ app/jobs/page.js
@@ -0,0 +1,12 @@
+export default function Jobs() {
+  return (
+    <div>
+      <header></header>
+      <main className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+        <h1>This is Jobs page</h1>
+        <article>Job advertisements are coming...</article>
+      </main>
+      <footer></footer>
+    </div>
+  );
+}
