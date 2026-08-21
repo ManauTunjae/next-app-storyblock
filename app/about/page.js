bb1021ab35469ea82282d207155aa4e63803e083
ManauTunjae <manau.tunjae@hotmail.com>
2026-08-21 09:53:07 +0200

    feat: implement a header and description in about page

---
+++ app/about/page.js
@@ -0,0 +1,15 @@
+export default function About() {
+  return (
+    <div>
+      <header></header>
+      <main className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+        <h1>This is About page</h1>
+        <article>
+          My name is Manau and this page is all about me. I am a fullstack
+          developer whos love to solve web prolems.
+        </article>
+      </main>
+      <footer></footer>
+    </div>
+  );
+}
